538a8f402ce89ea4f407657a5dfa30c8f69bbaf3
pelvinaydn <pelvin16@gmail.com>
2021-05-07 03:28:04 +0300

    7.05

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -54,10 +54,10 @@ namespace Business.Concrete
         [CacheAspect]
         public IDataResult<List<Product>> GetAll()
         {
-            if (DateTime.Now.Hour == 1)
-            {
-                return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
-            }
+            //if (DateTime.Now.Hour == 1)
+            //{
+            //    return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
+            //}
             return new SuccessDataResult<List<Product>>(_productDal.GetAll(), Messages.ProductsListed);
 
         }
